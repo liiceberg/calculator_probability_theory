7926f75113e78e995919392176f584df09efc3fe
liiceberg <2aysylu2003@gmail.com>
2023-10-02 18:06:54 +0300

    bug fixed

--- app/src/main/java/ru/kpfu/itis/gimaletdinova/alculatorprobabilitytheory/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/gimaletdinova/alculatorprobabilitytheory/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.kpfu.itis.gimaletdinova.alculatorprobabilitytheory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.kpfu.itis.gimaletdinova.alculatorprobabilitytheory.databinding.ActivityMainBinding
@@ -9,10 +10,10 @@ import ru.kpfu.itis.gimaletdinova.alculatorprobabilitytheory.databinding.Activit
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
     }
 }
